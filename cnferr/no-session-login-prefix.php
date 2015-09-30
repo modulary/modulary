173b73849c60d756e697b2cf59a3e2cd45bab59b
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-09-30 01:23:52 -0400

    One more glitch! Must improve myself!!!!

--- cnferr/no-session-login-prefix.php
+++ cnferr/no-session-login-prefix.php
@@ -3,7 +3,7 @@
 <h1>Prefix of Login Data in Session Variables Missing</h1>
 
 <p>You can fix this by editing the file: <b><?php mdlr_basefile(); ?></b>
-and defining the variable <b>$mdlr_anchr->param->xxx</b> to contain this information</b></p>
+and defining the variable <b>$mdlr_anchr->param->ssnp_login</b> to contain this information</b></p>
 
 
 </body></html>
